81646049baec1ccc3bf3c4cffdf4463346f687e1
andy reyes <11710reyes@gmail.com>
2019-02-03 16:27:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def intitialize(name, artist_name)
-    @name = name
-    @artist_name = artist_name
-    @@all << self
-  end 
   
   def self.all
     @@all
